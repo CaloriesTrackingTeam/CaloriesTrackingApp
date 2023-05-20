77799def42fc0b0f173e3de4e7a1eb8d0ae98fbb
Thinh-Tran-0902 <tranthinh5801@gmail.com>
2023-05-20 14:42:08 +0700

    creating login activity

---
+++ app/src/main/java/com/example/caloriestracking/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.caloriestracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Build;
+import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        /*Button btnface = findViewById(R.id.loginFaceButton);
+
+        // Storing the drawable location in a local value
+        Drawable mImage = getDrawable(R.drawable.facebook_icon);
+
+        // Compounding the button with drawables
+        // Here image will be at the left of the text.
+        btnface.setCompoundDrawablesWithIntrinsicBounds(mImage, null, null, null);*/
+
+    }
+}
