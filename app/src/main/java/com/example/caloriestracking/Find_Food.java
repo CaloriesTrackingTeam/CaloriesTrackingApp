bbb216fff8cbb2bea951c4e1933b15c3b4390f0a
BlueEyesssss <vuamessi9@gmail.com>
2023-05-25 10:29:35 +0700

    finish find food page

--- app/src/main/java/com/example/caloriestracking/Find_Food.java
+++ app/src/main/java/com/example/caloriestracking/Find_Food.java
@@ -1,16 +1,21 @@
 package com.example.caloriestracking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.caloriestracking.adapter.FoodAdapter;
 import com.example.caloriestracking.model.Food;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +30,28 @@ public class Find_Food extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_food);
 
+        BottomNavigationView btv = findViewById(R.id.bottom_nav);
+        btv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.ac_home){
+                    System.out.println("btv_ac_favorite_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                } else if(item.getItemId() == R.id.ac_search){
+                    System.out.println("btv_ac_search_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                }else if(item.getItemId() == R.id.ac_favorite){
+                    System.out.println("btv_ac_favorite_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                }else if(item.getItemId() == R.id.ac_user_page){
+                    System.out.println("btv_ac_user_page_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                }
+                return true;
+            }
+        });
+
+        //set up reccyle view
         rcv = findViewById(R.id.rcv_Food);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);   //dạng cột và có 2 cột
         rcv.setLayoutManager(gridLayoutManager);
@@ -32,6 +59,7 @@ public class Find_Food extends AppCompatActivity {
         FoodAdapter foodAdapter = new FoodAdapter(getListFood());
         rcv.setAdapter(foodAdapter);
 
+        //set up search icon click
         ImageView iconSearch = findViewById(R.id.iconSearch);
         iconSearch.setOnClickListener(new View.OnClickListener() {
             @Override
