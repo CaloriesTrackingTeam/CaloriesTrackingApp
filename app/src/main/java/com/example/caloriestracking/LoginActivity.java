73f262a15ee066073974a23be81671ba29ddc1b6
BlueEyesssss <vuamessi9@gmail.com>
2023-05-27 11:41:36 +0700

    re-work register3 + fix login page

--- app/src/main/java/com/example/caloriestracking/LoginActivity.java
+++ app/src/main/java/com/example/caloriestracking/LoginActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,35 +19,37 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btnFace = (Button)findViewById(R.id.loginFaceButton);
-        Button btnGoogle = (Button)findViewById(R.id.loginGoogleButton);
+        LinearLayout btnFace = findViewById(R.id.loginFaceButton);
+        LinearLayout btnGoogle = findViewById(R.id.loginGoogleButton);
         Button btnLogin = (Button)findViewById(R.id.loginButton);
         Button btnForgot = (Button)findViewById(R.id.forgotpasswordButton);
         Button btnSignUp = (Button)findViewById(R.id.signupButton);
 
         // to login Facebook activity
-        /*btnFace.setOnClickListener(new View.OnClickListener() {
+        btnFace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, [...].class));
+                System.out.println("btnFace lick");
+                //startActivity(new Intent(LoginActivity.this, [...].class));
             }
-        });*/
+        });
 
         // to login Google activity
-        /*btnGoogle.setOnClickListener(new View.OnClickListener() {
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, [...].class));
+                System.out.println("btnGoogle lick");
+                //startActivity(new Intent(LoginActivity.this, [...].class));
             }
-        });*/
+        });
 
         // to home activity
-        /*btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, [...].class));
+                //startActivity(new Intent(LoginActivity.this, [...].class));
             }
-        });*/
+        });
 
         // to Forgot Password activity
         btnForgot.setOnClickListener(new View.OnClickListener() {
