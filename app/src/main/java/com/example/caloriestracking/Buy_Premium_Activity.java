cca63b2d811a502ae16680d413fff0702933221d
Thinh-Tran-0902 <tranthinh5801@gmail.com>
2023-06-03 18:18:54 +0700

    complete layout of Subcription of user and buy sub

---
+++ app/src/main/java/com/example/caloriestracking/Buy_Premium_Activity.java
@@ -0,0 +1,45 @@
+package com.example.caloriestracking;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Buy_Premium_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_premium);
+
+        Button buymonthbtn = findViewById(R.id.buyMonthlyButton);
+        Button buyannualbtn = findViewById(R.id.buyAnnualButton);
+
+        BottomNavigationView btv = findViewById(R.id.bottom_nav2);
+
+        btv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.ac_home){
+                    System.out.println("btv_ac_favorite_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                } else if(item.getItemId() == R.id.ac_search){
+                    System.out.println("btv_ac_search_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                }else if(item.getItemId() == R.id.ac_favorite){
+                    System.out.println("btv_ac_favorite_click");
+                    //startActivity(new Intent(Find_Food.this, [home].class));
+                }else if(item.getItemId() == R.id.ac_user_page){
+                    System.out.println("btv_ac_user_page_click");
+                    startActivity(new Intent(Buy_Premium_Activity.this, User_Profile_Activity.class));
+                }
+                return true;
+            }
+        });
+    }
+}
